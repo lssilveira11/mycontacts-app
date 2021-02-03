6c49c1e0f8562d616754e5a3be3528bb3d2d2010
Lucas <lssilveira11@gmail.com>
2021-02-02 22:46:25 -0300

    Component separation

---
+++ src/components/contacts/ContactItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="row justify-content-between align-items-center">
+    <div class="col-auto">
+      <h5>{{ contact.name }}</h5>
+      <small class="d-block">{{ contact.email }}</small>
+      <small class="d-block">{{ contact.phone }}</small>
+    </div>
+    <div class="col-auto">
+      <div
+        class="btn-group"
+        role="group"
+        aria-label="Button group with nested dropdown"
+      >
+        <button type="button" class="btn btn-outline-secondary">Details</button>
+
+        <div class="btn-group" role="group">
+          <button
+            id="btnGroupDrop1"
+            type="button"
+            class="btn btn-outline-secondary dropdown-toggle"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          ></button>
+          <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+            <a class="dropdown-item" href="#">Edit</a>
+            <a class="dropdown-item text-danger" href="#">Delete</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContactItem",
+  components: {},
+  props: {
+    contact: Object,
+  },
+};
+</script>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">MyContacts</a>
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+      New Contact
+    </button>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  components: {}
+}
+</script>
